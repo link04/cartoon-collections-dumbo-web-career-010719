35094c23a464bd90068613f85eca092c0a1dd0e0
Maximo Bautista <max.bautista97@gmail.com>
2018-12-25 16:33:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,10 +14,9 @@ def long_planeteer_calls(array_of_calls)
   array_of_calls.any? {|word| word.length > 4}
 end
 
-def find_the_cheese
+def find_the_cheese(array_of_strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
-  
+  array_of_strings.include?
   
 end
