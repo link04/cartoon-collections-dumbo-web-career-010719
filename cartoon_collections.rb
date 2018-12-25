da39ecadffa23db7b4b91bed8ed3550be31cc711
Maximo Bautista <max.bautista97@gmail.com>
2018-12-25 16:32:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,21 +1,23 @@
-def roll_call_dwarves(array_of_dwarves)# code an argument here
+def roll_call_dwarves(array_of_dwarves)
   array_of_dwarves.each_with_index { |item, index|
     puts "#{index + 1 } #{item}"
   }
 end
 
-def summon_captain_planet(arra_planeteer_calls)# code an argument here
+def summon_captain_planet(arra_planeteer_calls)
   arra_planeteer_calls.collect do |item|
     item.capitalize + "!"
   end
 end
 
-def long_planeteer_calls(array_of_calls)# code an argument here
+def long_planeteer_calls(array_of_calls)
   array_of_calls.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  
   
 end
