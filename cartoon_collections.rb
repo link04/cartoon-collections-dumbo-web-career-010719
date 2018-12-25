edfefe23864ec83448405aa231d856387068ef5a
Maximo Bautista <max.bautista97@gmail.com>
2018-12-25 16:38:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,6 @@ def find_the_cheese(array_of_strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array_of_strings.each do |item|
-    cheese_types.include?(cheese) 
+    cheese_types.include?(item) 
   end
 end
